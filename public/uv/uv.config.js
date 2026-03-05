3b4ea9e1d6c84e8513aa353c45b6945fbda674ab
jlsims4681 <jlsims2@students.wcpss.net>
2026-03-05 18:53:33 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -9,4 +9,7 @@ self.__uv$config = {
 	bundle: "/uv/uv.bundle.js",
 	config: "/uv/uv.config.js",
 	sw: "/uv/uv.sw.js",
+if (typeof BareMux !== 'undefined') {
+    const connection = new BareMux.BareMuxConnection('/baremux/worker.js');
+    connection.setTransport('/epoxy/index.mjs', [{ wisp: 'wss://wisp.mercurywork.shop/' }]);
 };
